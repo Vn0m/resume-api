ba7142041219b48f6bfa384fb192ca864422fccc
Vn0m <juanpapolo123@gmail.com>
2024-01-05 21:32:51 -0500

    add delete and put method

--- routes/api.js
+++ routes/api.js
@@ -21,6 +21,30 @@ router.get('/experience', (req, res) => {
     res.status(200).json(experiences);
 });
 
+router.put("/:id", (req, res) => {
+    const expIndex = parseInt(req.params.id);
+    if (expIndex >= 0 && expIndex < experiences.length) {
+      // Update the experience at the found index
+      experiences[expIndex] = { ...experiences[expIndex], ...req.body };
+      res.status(200).json({ message: "Experiences updated successfully" });
+    } else {
+      res.status(404).json({ message: "Experiences not found" });
+    }
+  });
+  
+  router.delete("/:id", (req, res) => {
+    const expIndex = parseInt(req.params.id);
+  
+    if (expIndex >= 0 && expIndex < experiences.length) {
+      // Delete the experience at the found index
+      // delete one element at the  expIndex
+      experiences.splice(expIndex, 1);
+      res.status(200).json({ message: "Experience deleted successfully" });
+    } else {
+      res.status(404).json({ message: "Experience not found" });
+    }
+  });
+
 router.post('/experience', (req, res) => {
     const newExperience = req.body;
     experiences.push(newExperience);
